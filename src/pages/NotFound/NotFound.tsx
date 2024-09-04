83d26d34bf1c495615366eb14b5534fb683d93b3
maks <krivorukov2007@mail.ru>
2024-09-04 22:48:31 +0300

    Styled not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+import styles from './NotFound.module.scss'
 
 
 const NotFound = () => {
     return (
-        <div>
-            Page Not Found
+        <div className={styles.notFound}>
+            <h1>Sorry this page not found</h1>
+            <Link className={styles.notFound__link} to={'/'}>Go to Home</Link>
         </div>
     );
 };
